7b4fc0be96957bffe6ce74d0b513ab6f87a7feb9
Lewis Nixon <lewcfn@gmail.com>
2017-09-17 12:07:25 +0200

    install Apollo, fetch data and bind to Linkslist component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App/App';
+import registerServiceWorker from './registerServiceWorker'
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// 1
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+// 2
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj7oidriu00nj0179x2ns6aj6'
+})
+
+// 3
+const client = new ApolloClient({
+  networkInterface
+})
+
+// 4
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+)
+registerServiceWorker()
