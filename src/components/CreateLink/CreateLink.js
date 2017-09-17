8bf7b5f21152b28ba13e8645e3d626738819acf1
Lewis Nixon <lewcfn@gmail.com>
2017-09-17 18:56:02 +0200

    adding more shit

--- src/components/CreateLink/CreateLink.js
+++ src/components/CreateLink/CreateLink.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { graphql, gql } from 'react-apollo'
 import { GC_USER_ID } from '../../constants'
+import { ALL_LINKS_QUERY } from '../Linklist/Linklist'
 
 class CreateLink extends Component {
 
@@ -50,8 +51,17 @@ class CreateLink extends Component {
         description,
         url,
         postedById
+      },
+      update: (store, { data: { createLink } }) => {
+        const data = store.readQuery({ query: ALL_LINKS_QUERY })
+        data.allLinks.splice(0,0,createLink)
+        store.writeQuery({
+          query: ALL_LINKS_QUERY,
+          data
+        })
       }
     })
+
     this.props.history.push(`/`)
   }
 }
